35ed229b3e5307d00df778a50b60d842ed136487
fim4re <fimareyassine@gmail.com>
2024-04-19 08:56:26 -0700

    commit for the task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
 # Using puppet, install flask.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
