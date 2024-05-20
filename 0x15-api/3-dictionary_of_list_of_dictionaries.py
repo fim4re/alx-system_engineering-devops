287d463ac6be07b7f7842fdc9780df6aa5805465
fim4re <fimareyassine@gmail.com>
2024-05-20 06:46:26 -0700

    commit for the task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""Exports to-do list information employee ID to JSON format."""
+"""Exports information of all employees to JSON"""
 import json
 import requests
-import sys
 
 if __name__ == "__main__":
-    # Get the employee ID from the command-line
-    user_id = sys.argv[1]
-    # Fetch the list of all users
+    # Fetch user information and to-do lists
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    users = requests.get(url + "users").json()
 
     # Create a dictionary containing to-do list
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
